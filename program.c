6789090557cb1ac083e16fec4825275ca54aaa28
0AkshathaV <akshatha.v@s.amity.edu>
2025-02-12 14:25:28 +0530

    sub values

--- program.c
+++ program.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main()
-{   int a=4, b=6;
-	printf("Sum of two numbers is:"a+b)
-              return 0
+{   int a=4; b=6;
+	printf("Sum of two numbers is: %d",a+b);
+printf("sub of two numbers is: %d",a-b);
+}
